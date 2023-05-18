3478ab193d93d3f35f382ac361ef23041b6bc4a5
isabelcoboruizazuaga <59738137+isabelcoboruizazuaga@users.noreply.github.com>
2023-05-18 07:53:11 +0200

    registro

--- src/app/partials/registro/registro.component.ts
+++ src/app/partials/registro/registro.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.scss']
 })
-export class RegistroComponent {
-
+export class RegistroComponent implements OnInit {
+  constructor(
+    public authService: AuthService
+  ) { }
+  ngOnInit() { }
 }
