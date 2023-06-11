de6b75e7ce218d96f5cd58695bf77a4204d1d5bc
isabelcoboruizazuaga <59738137+isabelcoboruizazuaga@users.noreply.github.com>
2023-06-11 16:48:48 +0200

    media querys landing-page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,4 +9,18 @@ import { Component } from '@angular/core';
 
 export class LandingPageComponent {
 
+  /**
+   * This is a constructor function that takes in a router object as a parameter and assigns it to a
+   * private property.
+   * @param {Router} router - Used to navigate between different views or components.
+   */
+  constructor(private router: Router) {
+  }
+
+  /**
+   * This function navigates the user to the registration page.
+   */
+  irSesion() {
+    this.router.navigate(['/registro']);
+  }
 }
